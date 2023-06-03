dd6cab8a504f43c5ea319eb398c3a1f16f5f4cd7
DwarfPorter <mrvlmor@gmail.com>
2023-06-03 21:20:40 +0300

    для проталкивания тестов

--- Workshop/ArrayUtil.cs
+++ Workshop/ArrayUtil.cs
@@ -10,10 +10,6 @@ namespace Workshop
     {
         public static int SecondMax(int[] array)
         {
-            /* Здесь надо написать алгоритм поиска второго максимума, пример: 
-             * массив: [2, 4, 5, 3], результат: 4
-             * массив: [2, 6, 5, 3], результат: 5
-             */
             int max = 0;
             for(int i = 0; i < array.Length; i++)
             {
